548a6cf4509975d8a7408f7f6263d840590d81da
kusari-k <s15426@salesio-sp.ac.jp>
2020-01-13 02:17:55 +0900

    tst

--- script.sh
+++ script.sh
@@ -16,7 +16,8 @@ echo "domain=$domain"
 echo "global_ip=$global_ip"
 
 #make default smb.conf
-echo -e """anonymous_enable=NO
+echo -e """
+anonymous_enable=NO
 local_enable=YES
 write_enable=YES
 local_umask=022
@@ -24,27 +25,28 @@ dirmessage_enable=YES
 xferlog_enable=YES
 connect_from_port_20=YES
 xferlog_std_format=YES
-chroot_local_user=YES
-chroot_list_enable=YES
 listen=YES
 listen_ipv6=NO
-
 pam_service_name=vsftpd
 userlist_enable=YES
-tcp_wrappers=YES""">vsftpd.conf
+""">vsftpd.conf
 
 #set private setting
-echo -e """chroot_local_user=YES
+echo -e """
+chroot_local_user=YES
 chroot_list_enable=YES
+chroot_list_file=/etc/vsftpd/chroot_list
 pasv_address=$global_ip
 rsa_cert_file=/etc/letsencrypt/live/ftp.$domain/fullchain.pem
 rsa_private_key_file=/etc/letsencrypt/live/ftp.$domain/privkey.pem
 ssl_enable=YES
+pasv_enable=YES
 pasv_min_port=10000
 pasv_max_port=10050
 require_ssl_reuse=NO
 user_config_dir=/etc/vsftpd/vsftp_user_conf
 allow_writeable_chroot=YES
+
 """>>vsftpd.conf
 cat setting_vsftp.log |awk -F ":" -f script.awk
 wait
