cdd7ff2fa44c7268b5aafa804e0d2099ac543154
kusari-k <s15426@salesio-sp.ac.jp>
2020-02-13 00:38:23 +0900

    add network setting

--- script.sh
+++ script.sh
@@ -7,23 +7,29 @@ sed -z s/.*#samba//  setting.txt |
 	\sed '1d' >setting_samba.log
 
 #make default smb.conf
-echo -e "[global]">smb.conf
-echo -e "\tworkgroup = WORKGROUP">>smb.conf
-echo -e "\tsecurity = user">>smb.conf
-echo -e "\tpassdb backend = tdbsam">>smb.conf
-echo -e "\tprinting = cups">>smb.conf
-echo -e "\tprintcap name = cups">>smb.conf
-echo -e "\tload printers = no">>smb.conf
-echo -e "\tcups options = raw">>smb.conf
-echo -e "\twins support = yes">>smb.conf
+echo -e """[global]
+\tworkgroup = WORKGROUP
+\tsecurity = user
+\tpassdb backend = tdbsam
+\tprinting = cups
+\tprintcap name = cups
+\tload printers = no
+\tcups options = raw
+\twins support = yes""">>smb.conf
 
 #set private setting
 cat setting_samba.log |awk -F ":" -f script.awk
 wait
 
+#set network setting
+echo -e """networks:
+  default:
+    external:
+      name: default_bridge
+"""
+
 #write files
 echo -e "\nENTRYPOINT [\"/usr/local/bin/run.sh\"]">>Dockerfile
-echo -e "    image: samba\n    container_name: samba">>docker-compose.yml
 
 #cleaning
 rm *.log
