137148d9c1dd3f609a145470a5f5cee1cd061845
HNTKAR <45356209+HNTKAR@users.noreply.github.com>
2023-06-18 04:16:13 +0900

    fix radis config

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
-if [ ! -e /var/www/nextcloud/nextcloud/config/config.php ];then
+if [ ! -e /var/www/nextcloud/config/config.php ];then
 echo "<?php
 \$CONFIG = array (
-);" >/var/www/nextcloud/nextcloud/config/config.php
+);" >/var/www/nextcloud/config/config.php
 fi
 
-if [ $(grep default_phone_region /var/www/nextcloud/nextcloud/config/config.php |wc -l) -eq 0 ];then
-    sed -i.old -e "/^\$CONFIG/a 'default_phone_region' =>'JP'," /var/www/nextcloud/nextcloud/config/config.php
+if [ $(grep default_phone_region /var/www/nextcloud/config/config.php |wc -l) -eq 0 ];then
+    sed -i.old -e "/^\$CONFIG/a 'default_phone_region' =>'JP'," /var/www/nextcloud/config/config.php
 fi
 
 exec nginx -c /etc/nginx/nginx_user.conf
--- redis/run.sh
+++ redis/run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
-if [ ! -e /var/www/nextcloud/nextcloud/config/config.php ];then
+if [ ! -e /var/www/nextcloud/config/config.php ];then
 echo "<?php
 \$CONFIG = array (
-);" >/var/www/nextcloud/nextcloud/config/config.php
+);" >/var/www/nextcloud/config/config.php
 fi
 
-if [ $(grep Memcache /var/www/nextcloud/nextcloud/config/config.php |wc -l) -eq 0 ];then
-    sed -i.old -e "/^\$CONFIG/a 'memcache.local' => '\\\OC\\\Memcache\\\Redis',\n'memcache.distributed' => '\\\OC\\\Memcache\\\Redis',\n'redis' => [\n'host'     => '/sock/redis.sock',\n'port'     => 0,\n]," /var/www/nextcloud/nextcloud/config/config.php
+if [ $(grep Memcache /var/www/nextcloud/config/config.php |wc -l) -eq 0 ];then
+    sed -i.old -e "/^\$CONFIG/a 'filelocking.enabled' => true,\n'memcache.locking' => '\\\OC\\\Memcache\\\Redis',\n'redis'    => array(\n'host'     => '/sock/redis.sock',\n'port'     => 0,\n'timeout'  =>0.0,\n)," /var/www/nextcloud/config/config.php
+    sed -i.old -e "/^\$CONFIG/a 'memcache.distributed' => '\\\OC\\\Memcache\\\Redis',\n'memcache.local' => '\\\OC\\\Memcache\\\Redis',\n'redis' => [\n'host'     => '/sock/redis.sock',\n'port'     => 0,\n]," /var/www/nextcloud/config/config.php
 fi
 
 rm -fr /sock/redis.sock 
