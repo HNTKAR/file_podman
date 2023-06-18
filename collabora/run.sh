437b422f7232e16e82af95536fc8af8c4a636d33
HNTKAR <45356209+HNTKAR@users.noreply.github.com>
2023-06-19 01:04:58 +0900

    add collabora

---
+++ collabora/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+exec sudo -u cool coolwsd --version --o:sys_template_path=/opt/cool/systemplate --o:child_root_path=/opt/cool/child-roots --o:file_server_root_path=/usr/share/coolwsd
