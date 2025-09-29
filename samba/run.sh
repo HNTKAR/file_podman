d41e058a1316773e65ade7b04a6dd71e8da5a58f
HNTKAR <45356209+HNTKAR@users.noreply.github.com>
2025-09-29 22:45:01 +0900

    fix run.sh

--- samba/run.sh
+++ samba/run.sh
@@ -11,17 +11,25 @@ chown $(id -u):$(id -u) -R /usr/V/$CONTAINER_NAME/{conf,db,logs}
 chmod 777 -R /usr/V/$CONTAINER_NAME/{conf,db,logs}
 chmod 777 /usr/V/$CONTAINER_NAME/share
 
-adduser -D sample
+if [ -f "/usr/V/$CONTAINER_NAME/db/passwd.bak" ]; then
+    sed -i -e "/home/d" /etc/passwd
+    grep "home" /usr/V/$CONTAINER_NAME/db/passwd.bak >> /etc/passwd
+fi
+
+if [ -f "/usr/V/$CONTAINER_NAME/db/group.bak" ]; then
+    sed -i -e "/sample/d" /etc/group
+    grep "samba" /usr/V/$CONTAINER_NAME/db/group.bak >> /etc/group
+fi
+
+if [ -f "/usr/V/$CONTAINER_NAME/db/passdb.tdb.bak" ]; then
+    rm /etc/samba/passdb.tdb
+    cp /usr/V/$CONTAINER_NAME/db/passdb.tdb.bak /etc/samba/passdb.tdb
+fi
+
 echo -e "password\npassword"|pdbedit --create --password-from-stdin --user sample --configfile $CONF
 
 touch /usr/V/$CONTAINER_NAME/db/passdb.tdb
-pdbedit -i tdbsam:/usr/V/$CONTAINER_NAME/db/passdb.tdb --configfile $CONF 2> /dev/null
-
-
-touch /usr/V/$CONTAINER_NAME/db/passwd.bak
-cat /usr/V/$CONTAINER_NAME/db/passwd.bak >> /etc/passwd
-sort -u -o /etc/passwd_sorted /etc/passwd
-sort -n -k 3 -t ":" -o /etc/passwd /etc/passwd_sorted
+# pdbedit -i tdbsam:/usr/V/$CONTAINER_NAME/db/passdb.tdb --configfile $CONF 2> /dev/null
 
 nmbd
 touch /usr/V/$CONTAINER_NAME/conf/smb.conf
