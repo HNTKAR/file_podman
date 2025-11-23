0d15665dd6975c91bc04c70791a49691173bc42a
HNTKAR <45356209+HNTKAR@users.noreply.github.com>
2025-11-23 15:44:37 +0900

    fix nginx config

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mkdir -p /V/{conf,logs,sites}
-cp /usr/local/share/sites/* /V/sites/
-chown $(id -u nginx):$(id -u nginx) -R /V/{conf,logs}
-chmod 777 -R /V/{conf,logs}
+cp -rf /usr/local/share/sites/* /V/sites/
+chown -R $(id -u nginx):$(id -u nginx) /V/{conf,logs,sites} 
+chmod -R 777 /V/{conf,logs,sites}
 
 touch /V/logs/access.log /V/logs/error.log /V/conf/nginx.conf
 tail -F /V/logs/error.log &
 tail -F /V/logs/access.log &
 
-exec nginx -g 'daemon off;' -c /etc/nginx/nginx-user.conf
+exec nginx -c /etc/nginx/nginx-user.conf -g 'daemon off;'
