f1ac6483c974ecd489cc147e8685e0f756016239
HNTKAR <45356209+HNTKAR@users.noreply.github.com>
2025-11-24 09:37:07 +0900

    add certmgr

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-mkdir -p /V/{conf,logs,sites}
+mkdir -p /V/{conf,logs,cert,sites}
 cp -rf /usr/local/share/sites/* /V/sites/
-chown -R $(id -u nginx):$(id -u nginx) /V/{conf,logs,sites} 
-chmod -R 777 /V/{conf,logs,sites}
+chown -R $(id -u nginx):$(id -u nginx) /V/{conf,logs,sites,cert} 
+chmod -R 777 /V/{conf,logs,sites,cert}
+
+if [ -f /V/cert/status ]; then
+        echo "certificates found."
+else
+        echo "certificates not found. Copy default certs."
+        cp $CERT_DIR/* /V/cert/
+        echo "$(date) certificates copied from $CERT_DIR to /V/cert" > /V/cert/status
+fi
 
 touch /V/logs/access.log /V/logs/error.log /V/conf/nginx.conf
 tail -F /V/logs/error.log &
